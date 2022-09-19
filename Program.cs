13230c246641893a956540967b18e472918f849b
IgorZhil <103311402+IgorZhil@users.noreply.github.com>
2022-09-19 15:44:20 +0500

    First test added

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿//Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых <=3 символам
+
+
+
+using System;
+
+string[] array = // Создаём массив;
+{
+    "1234",
+    "1567",
+    "-2",
+    "computer science"
+
+};
+
+var result = new string[array.Length]; // вводим переменные 
+var realSize = 0;  // массива;
+foreach (var value in array)  // Вводим счетчики, которые считают длину массива из строки;
+{
+    if (value.Length <= 3)  // Проверяем длинну массива; Если длинна массива <=3;
+    {
+        result[realSize] = value; // Перезапускаем цикл заново;
+        realSize++;
+    }
+}
+
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));  // Выводим в консоль результаты;
+
+
